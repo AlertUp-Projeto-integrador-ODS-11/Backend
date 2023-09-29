95f0a970166b719683ff0a6d767de2b13a6a1f10
ShomaraQuispe <shomaraclaudia@gmail.com>
2023-09-29 11:18:32 -0300

    Validator criado

---
+++ AlertUp/Validator/TemaValidator.cs
@@ -0,0 +1,21 @@
+﻿using AlertUp.Model;
+using FluentValidation;
+
+namespace AlertUp.Validator
+{
+    public class TemaValidator : AbstractValidator<Tema>
+    {
+        public TemaValidator()
+        {
+            RuleFor(p => p.Titulo)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(100);
+
+            RuleFor(p => p.Descricao)
+                .NotEmpty()
+                .MinimumLength(5)
+                .MaximumLength(300);
+        }
+    }
+}
