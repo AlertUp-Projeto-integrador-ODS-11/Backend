311a621e2276bf5b5809c2e9b54a60bf3e0a127d
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-02 12:30:46 -0300

    Validator do User

---
+++ AlertUp/Validator/UserValidator.cs
@@ -0,0 +1,24 @@
+﻿using AlertUp.Model;
+using FluentValidation;
+
+
+namespace AlertUp.Validator
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        RuleFor(u => u.Nome)
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(100);
+
+        RuleFor(u => u.Email)
+                .NotEmpty()
+                .MinimumLength(5)
+                .MaximumLength(100);
+
+        RuleFor(u => u.Senha)
+                .NotEmpty()
+                .MinimumLength(5)
+                .MaximumLength(300);
+    }
+}
