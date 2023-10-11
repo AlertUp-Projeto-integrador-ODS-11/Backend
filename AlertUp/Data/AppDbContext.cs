723463c163d50386fdc7cad152d4ee607eb8007c
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-11 17:26:50 -0300

    Refatorando Configurations e AppDbContext

--- AlertUp/Data/AppDbContext.cs
+++ AlertUp/Data/AppDbContext.cs
@@ -37,39 +37,31 @@ namespace AlertUp.Data
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             var insertedEntries = this.ChangeTracker.Entries()
-            .Where(x => x.State == EntityState.Added)
-            .Select(x => x.Entity);
+                                   .Where(x => x.State == EntityState.Added)
+                                   .Select(x => x.Entity);
 
             foreach (var insertedEntry in insertedEntries)
             {
-
+                //Se uma propriedade da Classe Auditable estiver sendo criada. 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = DateTimeOffset.Now;
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
                 }
             }
 
             var modifiedEntries = ChangeTracker.Entries()
-            .Where(x => x.State == EntityState.Modified)
-            .Select(x => x.Entity);
+                       .Where(x => x.State == EntityState.Modified)
+                       .Select(x => x.Entity);
 
             foreach (var modifiedEntry in modifiedEntries)
             {
-
+                //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = DateTimeOffset.Now;
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
                 }
             }
-
             return base.SaveChangesAsync(cancellationToken);
-        }
-        
-        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
-        {
-            configurationBuilder
-                .Properties<DateTimeOffset>()
-                .HaveConversion<DateTimeOffsetConverter>();
         }
     }
 }
