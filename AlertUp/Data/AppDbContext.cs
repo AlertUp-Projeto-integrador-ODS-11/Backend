e44fd1974122310e3b1d6887ecf2d950c04f7ea4
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-11 16:34:24 -0300

    Refatorando DockerFile

--- AlertUp/Data/AppDbContext.cs
+++ AlertUp/Data/AppDbContext.cs
@@ -45,7 +45,7 @@ namespace AlertUp.Data
 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
@@ -58,7 +58,7 @@ namespace AlertUp.Data
 
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
